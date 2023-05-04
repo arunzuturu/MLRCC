c472121ffa94e91110c61fbd2434194082cfaef2
Arun Adithya <78028212+arunzuturu@users.noreply.github.com>
2023-05-04 12:26:27 +0530

    Updated Home View

---
+++ lib/features/home/views/nav_view.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mlrcc/theme/pallete.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
+import '../../../constants/ui_constants.dart';
+
+class HomeView extends ConsumerStatefulWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const HomeView(),
+      );
+  const HomeView({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<HomeView> {
+  int _currentIndex = 0;
+  int _page = 0;
+  final appbar = UIConstants.appBar();
+  void onPageChange(int index) {
+    setState(() {
+      _page = index;
+    });
+  }
+  List<Widget> _children = [];
+  @override
+  Widget build(BuildContext context) {
+    _children = [
+      Container(color: Colors.red,),
+      Container(color: Colors.blueAccent,),
+      Container(color: Colors.grey,),
+      Container(color: Colors.yellowAccent,),
+    ];
+    return Scaffold(
+      backgroundColor: Pallete.backgroundColor,
+      body: _children[_currentIndex],
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Pallete.backgroundColor,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 22.0, vertical: 12),
+            child: GNav(
+              onTabChange: (int index) {
+                setState(() {
+                  _currentIndex = index;
+                });},
+              gap: 8,
+              activeColor: Colors.white,
+              iconSize: 24,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              duration: const Duration(milliseconds: 500),
+              tabBackgroundColor: Pallete.accentColor,
+              color: Pallete.accentColor,
+              tabs: [
+                GButton(
+                  icon: LineIcons.home,
+                  text: 'Home',
+                  textStyle: TextStyle(color: Pallete.whiteColor),
+                ),
+                GButton(
+                  icon: LineIcons.pollH,
+                  text: 'Notice Board',
+                  textStyle: TextStyle(color: Pallete.whiteColor),
+                ),
+                GButton(
+                  icon: LineIcons.search,
+                  text: 'Explore',
+                  textStyle: TextStyle(color: Pallete.whiteColor),
+                ),
+                GButton(
+                  icon: LineIcons.user,
+                  text: 'Profile',
+                  textStyle:TextStyle(color: Pallete.whiteColor),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
